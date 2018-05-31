64cdb967a7595ae01eb2f002d598c289199189e0
16110339 <haris.angriawan@students.amikom.ac.id>
2018-06-01 05:19:28 +0700

    update2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,22 @@
 "use strict";
 
 
-
+$('.slider-active').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:['Back','Next'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
 
 
